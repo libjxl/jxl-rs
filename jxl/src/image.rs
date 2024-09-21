07fc6ce7a0be66adda76c65383fe9a50fa74419c
Luca Versari <veluca93@gmail.com>
2024-09-21 16:05:05 +0200

    Cleanup: fix pull_request_checks.sh, add missing headers.
    Also add `#[deny(unsafe_code)]` to ensure that unsafe code is easier to
    audit, and missing_safety_doc, undocumented_unsafe_blocks, unsafe_op_in_unsafe_fn.

--- jxl/src/image.rs
+++ jxl/src/image.rs
@@ -1,3 +1,8 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 use crate::error::{Error, Result};
 
 mod private {
--- jxl/src/lib.rs
+++ jxl/src/lib.rs
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#![deny(unsafe_code)]
 pub mod bit_reader;
 pub mod container;
 pub mod entropy_coding;
