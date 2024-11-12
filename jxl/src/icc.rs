42c6bdbc6720b953c31153f6725c1bb12f14b876
Wonwoo Choi <chwo9843@gmail.com>
2024-11-12 22:10:49 +0900

    Decode ICC profile stream

--- jxl/src/icc.rs
+++ jxl/src/icc.rs
@@ -3,23 +3,116 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+use std::io::Cursor;
+
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+
 use crate::bit_reader::*;
 use crate::entropy_coding::decode::Histograms;
-use crate::error::Error;
+use crate::error::{Error, Result};
 use crate::headers::encodings::*;
+use crate::util::tracing_wrappers::warn;
+
+mod header;
+mod stream;
+mod tag;
+
+use header::read_header;
+use stream::{read_varint_from_reader, IccStream};
+use tag::{read_single_command, read_tag_list};
 
 const ICC_CONTEXTS: usize = 41;
+const ICC_HEADER_SIZE: u64 = 128;
 
+fn read_icc_inner(stream: &mut IccStream) -> Result<Vec<u8>, Error> {
+    let output_size = stream.read_varint()?;
+    let commands_size = stream.read_varint()?;
+    if stream.bytes_read().saturating_add(commands_size) > stream.len() {
+        return Err(Error::InvalidIccStream);
+    }
+
+    // Simple check to avoid allocating too large buffer.
+    if output_size > (1 << 28) {
+        return Err(Error::IccTooLarge);
+    }
+
+    if output_size + 65536 < stream.len() {
+        return Err(Error::IccTooLarge);
+    }
+
+    // Extract command stream first.
+    let commands = stream.read_to_vec_exact(commands_size as usize)?;
+    let mut commands_stream = Cursor::new(commands);
+    // `stream` contains data stream from here.
+    let data_stream = stream;
+
+    // Decode ICC profile header.
+    let mut decoded_profile = read_header(data_stream, output_size)?;
+    if output_size <= ICC_HEADER_SIZE {
+        return Ok(decoded_profile);
+    }
+
+    // Convert to slice writer to prevent buffer from growing.
+    // `read_header` above returns buffer with capacity of `output_size`, so this doesn't realloc.
+    debug_assert_eq!(decoded_profile.capacity(), output_size as usize);
+    decoded_profile.resize(output_size as usize, 0);
+    let mut decoded_profile_writer = Cursor::new(&mut *decoded_profile);
+    decoded_profile_writer.set_position(ICC_HEADER_SIZE);
+
+    // Decode tag list.
+    let v = read_varint_from_reader(&mut commands_stream)?;
+    if let Some(num_tags) = v.checked_sub(1) {
+        if (output_size - ICC_HEADER_SIZE) / 12 < num_tags {
+            warn!(output_size, num_tags, "num_tags too large");
+            return Err(Error::InvalidIccStream);
+        }
+
+        let num_tags = num_tags as u32;
+        decoded_profile_writer
+            .write_u32::<BigEndian>(num_tags)
+            .map_err(|_| Error::InvalidIccStream)?;
+
+        read_tag_list(
+            data_stream,
+            &mut commands_stream,
+            &mut decoded_profile_writer,
+            num_tags,
+            output_size,
+        )?;
+    }
+
+    // Decode tag data.
+    // Will not enter the loop if end of stream was reached while decoding tag list.
+    while let Ok(command) = commands_stream.read_u8() {
+        read_single_command(
+            data_stream,
+            &mut commands_stream,
+            &mut decoded_profile_writer,
+            command,
+        )?;
+    }
+
+    // Validate output size.
+    let actual_len = decoded_profile_writer.position();
+    if actual_len != output_size {
+        warn!(output_size, actual_len, "ICC profile size mismatch");
+        return Err(Error::InvalidIccStream);
+    }
+
+    Ok(decoded_profile)
+}
+
+/// Read and decode ICC profile from `BitReader`.
+// TODO(tirr-c): Make this resumable.
 pub fn read_icc(br: &mut BitReader) -> Result<Vec<u8>, Error> {
     let len = u64::read_unconditional(&(), br, &Empty {})?;
     if len > 1u64 << 20 {
-        return Err(Error::ICCTooLarge);
+        return Err(Error::IccTooLarge);
     }
-    println!("Encoded len: {}", len);
 
     let histograms = Histograms::decode(ICC_CONTEXTS, br, /*allow_lz77=*/ true)?;
-
-    println!("{:#?}", histograms);
-
-    Ok(vec![])
+    let mut stream = IccStream::new(br, &histograms, len)?;
+    let profile = read_icc_inner(&mut stream)?;
+    stream.finalize()?;
+    Ok(profile)
 }
---
+++ jxl/src/icc/stream.rs
@@ -0,0 +1,172 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use std::io::{Read, Write};
+
+use byteorder::{ReadBytesExt, WriteBytesExt};
+
+use crate::bit_reader::*;
+use crate::entropy_coding::decode::{Histograms, Reader};
+use crate::error::{Error, Result};
+use crate::util::tracing_wrappers::{instrument, warn};
+
+fn read_varint(mut read_one: impl FnMut() -> Result<u8>) -> Result<u64> {
+    let mut value = 0u64;
+    let mut shift = 0;
+    while shift < 63 {
+        let b = read_one()?;
+        value |= ((b & 0x7f) as u64) << shift;
+        if b & 0x80 == 0 {
+            break;
+        }
+        shift += 7;
+    }
+    Ok(value)
+}
+
+pub(super) fn read_varint_from_reader(stream: &mut impl Read) -> Result<u64> {
+    read_varint(|| stream.read_u8().map_err(|_| Error::IccEndOfStream))
+}
+
+pub(super) struct IccStream<'br, 'buf, 'hist> {
+    br: &'br mut BitReader<'buf>,
+    reader: Reader<'hist>,
+    len: u64,
+    bytes_read: u64,
+    // [prev, prev_prev]
+    prev_bytes: [u8; 2],
+}
+
+impl<'br, 'buf, 'hist> IccStream<'br, 'buf, 'hist> {
+    pub(super) fn new(
+        br: &'br mut BitReader<'buf>,
+        histograms: &'hist Histograms,
+        len: u64,
+    ) -> Result<Self> {
+        let reader = histograms.make_reader(br)?;
+        Ok(Self {
+            br,
+            reader,
+            len,
+            bytes_read: 0,
+            prev_bytes: [0, 0],
+        })
+    }
+
+    pub fn len(&self) -> u64 {
+        self.len
+    }
+
+    pub fn bytes_read(&self) -> u64 {
+        self.bytes_read
+    }
+
+    pub fn remaining_bytes(&self) -> u64 {
+        self.len - self.bytes_read
+    }
+
+    fn get_icc_ctx(&self) -> u32 {
+        if self.bytes_read <= super::ICC_HEADER_SIZE {
+            return 0;
+        }
+
+        let [b1, b2] = self.prev_bytes;
+
+        let p1 = match b1 {
+            b'a'..=b'z' | b'A'..=b'Z' => 0,
+            b'0'..=b'9' | b'.' | b',' => 1,
+            0..=1 => 2 + b1 as u32,
+            2..=15 => 4,
+            241..=254 => 5,
+            255 => 6,
+            _ => 7,
+        };
+        let p2 = match b2 {
+            b'a'..=b'z' | b'A'..=b'Z' => 0,
+            b'0'..=b'9' | b'.' | b',' => 1,
+            0..=15 => 2,
+            241..=255 => 3,
+            _ => 4,
+        };
+
+        1 + p1 + 8 * p2
+    }
+
+    fn read_one(&mut self) -> Result<u8> {
+        if self.remaining_bytes() == 0 {
+            return Err(Error::IccEndOfStream);
+        }
+
+        let ctx = self.get_icc_ctx() as usize;
+        let sym = self.reader.read(&mut *self.br, ctx)?;
+        if sym >= 256 {
+            warn!(sym, "Invalid symbol in ICC stream");
+            return Err(Error::InvalidIccStream);
+        }
+        let b = sym as u8;
+
+        self.bytes_read += 1;
+        self.prev_bytes = [b, self.prev_bytes[0]];
+        Ok(b)
+    }
+
+    pub fn read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
+        if buf.len() > self.remaining_bytes() as usize {
+            return Err(Error::IccEndOfStream);
+        }
+
+        for b in buf {
+            *b = self.read_one()?;
+        }
+
+        Ok(())
+    }
+
+    pub fn read_to_vec_exact(&mut self, len: usize) -> Result<Vec<u8>> {
+        if len > self.remaining_bytes() as usize {
+            return Err(Error::IccEndOfStream);
+        }
+
+        let mut out = Vec::new();
+        out.try_reserve(len)?;
+
+        for _ in 0..len {
+            out.push(self.read_one()?);
+        }
+
+        Ok(out)
+    }
+
+    pub fn read_varint(&mut self) -> Result<u64> {
+        read_varint(|| self.read_one())
+    }
+
+    pub fn copy_bytes(&mut self, writer: &mut impl Write, len: usize) -> Result<()> {
+        if len > self.remaining_bytes() as usize {
+            return Err(Error::IccEndOfStream);
+        }
+
+        for _ in 0..len {
+            let b = self.read_one()?;
+            writer.write_u8(b).map_err(|_| Error::InvalidIccStream)?;
+        }
+
+        Ok(())
+    }
+
+    #[instrument(skip_all, err)]
+    pub fn finalize(self) -> Result<()> {
+        // Test entropy decoder checksum
+        self.reader.check_final_state()?;
+
+        // Check if all bytes are read
+        if self.bytes_read == self.len {
+            Ok(())
+        } else {
+            warn!("ICC stream is not fully consumed");
+            Err(Error::InvalidIccStream)
+        }
+    }
+}
