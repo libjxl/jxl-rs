c5d0fb13e3078a29c202e5033fa58259c5cae085
Moritz Firsching <firsching@google.com>
2024-11-25 10:03:47 +0100

    fmt

--- jxl_test_utils/src/lib.rs
+++ jxl_test_utils/src/lib.rs
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-
 #[macro_export]
 macro_rules! assert_almost_eq {
     ($left:expr, $right:expr, $max_error:expr $(,)?) => {
