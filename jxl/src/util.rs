38cbc699ef07901ec0b64a0189c75f79fa4f7d66
Moritz Firsching <firsching@google.com>
2024-12-24 10:18:54 +0100

    cleanup a few `allow(unused)`

--- jxl/src/util.rs
+++ jxl/src/util.rs
@@ -3,18 +3,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#[allow(unused)]
 pub mod test;
 
 mod bits;
-#[allow(unused)]
 mod concat_slice;
 mod log2;
 mod shift_right_ceil;
 pub mod tracing_wrappers;
 
 pub use bits::*;
-#[allow(unused)]
 pub use concat_slice::*;
 pub use log2::*;
 pub use shift_right_ceil::*;
