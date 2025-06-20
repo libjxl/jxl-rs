42c146b9bf59f9cc112ab0b5c3107d276c0b8c56
Sami Boukortt <sboukortt@google.com>
2025-06-20 16:27:46 +0200

    Write 16-bit PNGs when warranted

--- jxl_cli/src/enc/png.rs
+++ jxl_cli/src/enc/png.rs
@@ -5,6 +5,7 @@
 
 use jxl::decode::ImageData;
 use jxl::error::{Error, Result};
+use jxl::headers::bit_depth::BitDepth;
 
 use std::io::BufWriter;
 
@@ -18,7 +19,7 @@ fn png_color(num_channels: usize) -> Result<png::ColorType> {
     }
 }
 
-fn encode_png(image_data: ImageData<f32>, buf: &mut Vec<u8>) -> Result<()> {
+fn encode_png(image_data: ImageData<f32>, bit_depth: BitDepth, buf: &mut Vec<u8>) -> Result<()> {
     if image_data.frames.is_empty()
         || image_data.frames[0].channels.is_empty()
         || image_data.size.0 == 0
@@ -45,30 +46,53 @@ fn encode_png(image_data: ImageData<f32>, buf: &mut Vec<u8>) -> Result<()> {
     let w = BufWriter::new(buf);
     let mut encoder = png::Encoder::new(w, width as u32, height as u32);
     encoder.set_color(png_color(num_channels)?);
-    encoder.set_depth(png::BitDepth::Eight);
+    let eight_bits = bit_depth.bits_per_sample() <= 8;
+    encoder.set_depth(if eight_bits {
+        png::BitDepth::Eight
+    } else {
+        png::BitDepth::Sixteen
+    });
     if image_data.frames.len() > 1 {
         // TODO(szabadka): Handle error.
         let _ = encoder.set_animated(image_data.frames.len() as u32, 0);
     }
     let mut writer = encoder.write_header().unwrap();
     let num_pixels = height * width * num_channels;
-    let mut data: Vec<u8> = vec![0; num_pixels];
-    for frame in image_data.frames {
-        for y in 0..height {
-            for x in 0..width {
-                for c in 0..num_channels {
-                    data[(y * width + x) * num_channels + c] =
-                        frame.channels[c].as_rect().row(y)[x].clamp(0.0, 255.0) as u8;
+    if eight_bits {
+        let mut data: Vec<u8> = vec![0; num_pixels];
+        for frame in image_data.frames {
+            for y in 0..height {
+                for x in 0..width {
+                    for c in 0..num_channels {
+                        data[(y * width + x) * num_channels + c] =
+                            frame.channels[c].as_rect().row(y)[x].clamp(0.0, 255.0) as u8;
+                    }
                 }
             }
+            writer.write_image_data(&data).unwrap();
         }
-        writer.write_image_data(&data).unwrap();
+    } else {
+        let mut data: Vec<u8> = vec![0; 2 * num_pixels];
+        for frame in image_data.frames {
+            for y in 0..height {
+                for x in 0..width {
+                    for c in 0..num_channels {
+                        let pixel = (frame.channels[c].as_rect().row(y)[x] * 65535.0 / 255.0)
+                            .clamp(0.0, 65535.0) as u16;
+                        let index = 2 * ((y * width + x) * num_channels + c);
+                        data[index] = (pixel >> 8) as u8;
+                        data[index + 1] = (pixel & 0xFF) as u8;
+                    }
+                }
+            }
+            writer.write_image_data(&data).unwrap();
+        }
     }
     Ok(())
 }
 
-pub fn to_png(image_data: ImageData<f32>) -> Result<Vec<u8>> {
+pub fn to_png(image_data: ImageData<f32>, bit_depth: BitDepth) -> Result<Vec<u8>> {
     let mut buf = Vec::<u8>::new();
-    encode_png(image_data, &mut buf)?;
+    encode_png(image_data, bit_depth, &mut buf)?;
     Ok(buf)
 }
