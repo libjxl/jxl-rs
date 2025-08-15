57ceda3c9b7e53fb5721bd4448a37a94fe4d09a3
Sami Boukortt <sboukortt@google.com>
2025-08-15 16:30:28 +0200

    Support float samples in ConvertModularToF32Stage

--- jxl/src/render/stages/convert.rs
+++ jxl/src/render/stages/convert.rs
@@ -108,15 +108,12 @@ impl RenderPipelineStage for ConvertModularXYBToF32Stage {
 
 pub struct ConvertModularToF32Stage {
     channel: usize,
-    scale: f32,
+    bit_depth: BitDepth,
 }
 
 impl ConvertModularToF32Stage {
     pub fn new(channel: usize, bit_depth: BitDepth) -> ConvertModularToF32Stage {
-        // TODO(szabadka): Support floating point samples.
-        let bits_per_sample = bit_depth.bits_per_sample();
-        let scale = 1.0 / ((1u64 << bits_per_sample) - 1) as f32;
-        ConvertModularToF32Stage { channel, scale }
+        ConvertModularToF32Stage { channel, bit_depth }
     }
 }
 
@@ -124,9 +121,61 @@ impl std::fmt::Display for ConvertModularToF32Stage {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "convert modular data to F32 in channel {} with scale {}",
-            self.channel, self.scale
+            "convert modular data to F32 in channel {} with bit depth {:?}",
+            self.channel, self.bit_depth
         )
+    }
+}
+
+// Converts custom [bits]-bit float (with [exp_bits] exponent bits) stored as
+// int back to binary32 float.
+// TODO(sboukortt): SIMD
+fn int_to_float(input: &[i32], output: &mut [f32], bit_depth: &BitDepth) {
+    assert_eq!(input.len(), output.len());
+    let bits = bit_depth.bits_per_sample();
+    let exp_bits = bit_depth.exponent_bits_per_sample();
+    if bits == 32 {
+        assert!(exp_bits == 8);
+        for (&in_val, out_val) in input.iter().zip(output) {
+            *out_val = f32::from_bits(in_val as u32);
+        }
+        return;
+    }
+    let exp_bias = (1 << (exp_bits - 1)) - 1;
+    let sign_shift = bits - 1;
+    let mant_bits = bits - exp_bits - 1;
+    let mant_shift = 23 - mant_bits;
+    for (&in_val, out_val) in input.iter().zip(output) {
+        let mut f = in_val as u32;
+        let signbit = (f >> sign_shift) != 0;
+        f &= (1 << sign_shift) - 1;
+        if f == 0 {
+            *out_val = if signbit { -0.0 } else { 0.0 };
+            continue;
+        }
+        let mut exp = (f >> mant_bits) as i32;
+        let mut mantissa = f & ((1 << mant_bits) - 1);
+        mantissa <<= mant_shift;
+        // Try to normalize only if there is space for maneuver.
+        if exp == 0 && exp_bits < 8 {
+            // subnormal number
+            while (mantissa & 0x800000) == 0 {
+                mantissa <<= 1;
+                exp -= 1;
+            }
+            exp += 1;
+            // remove leading 1 because it is implicit now
+            mantissa &= 0x7fffff;
+        }
+        exp -= exp_bias;
+        // broke up the arbitrary float into its parts, now reassemble into
+        // binary32
+        exp += 127;
+        assert!(exp >= 0);
+        f = if signbit { 0x80000000 } else { 0 };
+        f |= (exp as u32) << 23;
+        f |= mantissa;
+        *out_val = f32::from_bits(f);
     }
 }
 
@@ -145,8 +194,13 @@ impl RenderPipelineStage for ConvertModularToF32Stage {
         _state: Option<&mut dyn std::any::Any>,
     ) {
         let (input, output) = &mut row[0];
-        for i in 0..xsize {
-            output[0][i] = input[0][i] as f32 * self.scale;
+        if self.bit_depth.floating_point_sample() {
+            int_to_float(&input[0][..xsize], &mut output[0][..xsize], &self.bit_depth);
+        } else {
+            let scale = 1.0 / ((1u64 << self.bit_depth.bits_per_sample()) - 1) as f32;
+            for i in 0..xsize {
+                output[0][i] = input[0][i] as f32 * scale;
+            }
         }
     }
 }
