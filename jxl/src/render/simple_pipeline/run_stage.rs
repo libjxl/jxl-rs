ecccc4c16f40f3446b37387bd4ecbe75bf7d4f4e
Luca Versari <veluca93@gmail.com>
2025-10-28 13:31:08 +0100

    Fix headers

--- jxl/src/render/simple_pipeline/run_stage.rs
+++ jxl/src/render/simple_pipeline/run_stage.rs
@@ -1,3 +1,8 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 use std::any::Any;
 
 use crate::{
