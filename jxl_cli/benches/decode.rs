3378117bc6ac8c1ba426217011a451dab04c3227
Martin Bruse <zondolfin@gmail.com>
2025-11-20 10:57:41 +0100

    Added criterion bench for decoding
    Defaults to benchmarking the files in the jxl/resourced directory, but can be controlled via JXL_FILES env variable.

---
+++ jxl_cli/benches/decode.rs
@@ -0,0 +1,68 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+use criterion::{BenchmarkId, Criterion, SamplingMode, criterion_group, criterion_main};
+use jxl::api::JxlDecoderOptions;
+use jxl_cli::dec::{decode_bytes, decode_header};
+use std::fs;
+use std::path::{Path, PathBuf};
+
+fn decode_benches(c: &mut Criterion) {
+    let mut group = c.benchmark_group("decode");
+    group.sampling_mode(SamplingMode::Flat);
+
+    let paths: Vec<PathBuf> = std::env::var("JXL_FILES").map_or_else(
+        |_| {
+            let root_test_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+                .join("..")
+                .join("jxl")
+                .join("resources")
+                .join("test");
+            [
+                &root_test_dir,
+                &root_test_dir.join("conformance_test_images"),
+            ]
+            .iter()
+            .flat_map(|path| {
+                fs::read_dir(path)
+                    .into_iter()
+                    .flatten()
+                    .filter_map(|e| e.ok())
+                    .map(|e| e.path())
+                    .filter(|p| p.is_file() && p.extension().is_some_and(|ext| ext == "jxl"))
+                    .collect::<Vec<PathBuf>>()
+            })
+            .collect()
+        },
+        |csv| csv.split(',').map(PathBuf::from).collect(),
+    );
+
+    for path in paths {
+        let bytes = fs::read(&path).unwrap();
+        let mut header_bytes = bytes.as_slice();
+        let header_decoder =
+            decode_header(&mut header_bytes, JxlDecoderOptions::default()).unwrap();
+        let pixel_count = header_decoder.basic_info().size.0 * header_decoder.basic_info().size.1;
+        group.throughput(criterion::Throughput::Elements(pixel_count as u64));
+        group.bench_with_input(
+            BenchmarkId::from_parameter(path.to_string_lossy()),
+            &path,
+            |b, _path| {
+                b.iter(|| {
+                    decode_bytes(&bytes, JxlDecoderOptions::default()).unwrap();
+                })
+            },
+        );
+    }
+
+    group.finish();
+}
+
+criterion_group!(
+    name = decode;
+    config = Criterion::default().sample_size(50);
+    targets = decode_benches
+);
+criterion_main!(decode);
