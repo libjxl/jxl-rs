72123f54c98043913d377d93dacf9a31e10379d0
Helmut Januschka <helmut@januschka.com>
2025-11-28 23:26:17 +0100

    Add copyright and license header to build.sh

--- jxl_wasm/build.sh
+++ jxl_wasm/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) the JPEG XL Project Authors. All rights reserved.
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 set -e
 
 echo "🔨 Building JXL WASM decoder..."
