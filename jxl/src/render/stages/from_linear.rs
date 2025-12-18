2ad08a388de7a27fef1b55690e5ac800c76a8f06
Helmut Januschka <helmut@januschka.com>
2025-12-18 16:55:32 +0100

    Add SIMD versions of transfer functions (#536)
    * Add SIMD versions of transfer functions
    
    Add bt709_to_linear_simd, pq_to_linear_simd, and linear_to_pq_simd
    using existing fast_powf_simd and eval_rational_poly_simd helpers.
    Update ToLinearStage and FromLinearStage to use SIMD versions.
    
    Related to #521
    
    * Add xsize parameter to SIMD transfer functions and use .take()
    
    Pass xsize to bt709_to_linear_simd, pq_to_linear_simd, and
    linear_to_pq_simd and use .take(xsize.div_ceil(D::F32Vec::LEN))
    to limit iteration instead of requiring the caller to pad the slice.

--- jxl/src/render/stages/from_linear.rs
+++ jxl/src/render/stages/from_linear.rs
@@ -77,7 +77,7 @@ fn from_linear_process(tf: &TransferFunction, xsize: usize, row: &mut [&mut [f32
         }
         TransferFunction::Pq { intensity_target } => {
             for row in row {
-                tf::linear_to_pq(intensity_target, &mut row[..xsize]);
+                tf::linear_to_pq_simd(d, intensity_target, xsize, row);
             }
         }
         TransferFunction::Hlg {
