libjxl/jxl-rs

commit 99e245a756f6e88dbad0939ea59565756af69716

Add a SmallVec type and optimize the low mem render pipeline.
Also apply a few other optimizations to the render pipeline, to reduce
allocations and avoid repeated computation.

Roughly 25.5% speedup on Android, 10.5% on x86 desktop on a large test
image.

jxl/src/render/low_memory_pipeline/mod.rs
@@ -39,7 +39,7 @@ pub struct LowMemoryRenderPipeline {
     save_buffer_info: Vec<Option<SaveStageBufferInfo>>,
     // The input buffer that each channel of each stage should use.
     // This is indexed both by stage index (0 corresponds to input data, 1 to stage[0], etc) and by
-    // channel index (as only used channels have a buffer).
+    // index *of those channels that are used*.
     stage_input_buffer_index: Vec<Vec<(usize, usize)>>,
     // Tracks whether we already rendered the padding around the core frame (if any).
     padding_was_rendered: bool,
@@ -58,6 +58,10 @@ pub struct LowMemoryRenderPipeline {
     // Pre-filled opaque alpha buffers for stages that need fill_opaque_alpha.
     // Indexed by stage index; None if stage doesn't need alpha fill.
     opaque_alpha_buffers: Vec<Option<RowBuffer>>,
+    // Sorted indices to call get_distinct_indices.
+    sorted_buffer_indices: Vec<Vec<(usize, usize, usize)>>,
+    // For each channel, buffers that could be reused to store group data for that channel.
+    scratch_channel_buffers: Vec<Vec<OwnedRawImage>>,
 }
 
 impl LowMemoryRenderPipeline {
@@ -184,8 +188,10 @@ impl LowMemoryRenderPipeline {
                 }
             }
             if self.shared.num_passes <= neigh_complete_passes {
-                for b in self.input_buffers[g].data.iter_mut() {
-                    *b = None;
+                for (c, b) in self.input_buffers[g].data.iter_mut().enumerate() {
+                    if let Some(b) = std::mem::take(b) {
+                        self.scratch_channel_buffers[c].push(b);
+                    }
                 }
             }
         }
@@ -350,6 +356,28 @@ impl RenderPipeline for LowMemoryRenderPipeline {
             }
         }
 
+        for (s, ibi) in stage_input_buffer_index.iter_mut().enumerate() {
+            let mut filtered = vec![];
+            for c in 0..nc {
+                if shared.stages[s].uses_channel(c) {
+                    filtered.push(ibi[c]);
+                }
+            }
+            *ibi = filtered;
+        }
+
+        let sorted_buffer_indices = (0..shared.stages.len())
+            .map(|s| {
+                let mut v: Vec<_> = stage_input_buffer_index[s]
+                    .iter()
+                    .enumerate()
+                    .map(|(i, (outer, inner))| (*outer, *inner, i))
+                    .collect();
+                v.sort();
+                v
+            })
+            .collect();
+
         Ok(Self {
             input_buffers,
             stage_input_buffer_index,
@@ -367,11 +395,16 @@ impl RenderPipeline for LowMemoryRenderPipeline {
             shared,
             downsampling_for_stage,
             opaque_alpha_buffers,
+            sorted_buffer_indices,
+            scratch_channel_buffers: (0..nc).map(|_| vec![]).collect(),
         })
     }
 
     #[instrument(skip_all, err)]
     fn get_buffer<T: ImageDataType>(&mut self, channel: usize) -> Result<Image<T>> {
+        if let Some(b) = self.scratch_channel_buffers[channel].pop() {
+            return Ok(Image::from_raw(b));
+        }
         let sz = self.shared.group_size_for_channel(channel, T::DATA_TYPE_ID);
         Image::<T>::new(sz)
     }

jxl/src/render/low_memory_pipeline/render_group.rs
@@ -16,10 +16,14 @@ use crate::{
             run_stage::ExtraInfo,
         },
     },
-    util::{ShiftRightCeil, tracing_wrappers::*},
+    util::{ShiftRightCeil, SmallVec, tracing_wrappers::*},
 };
 
 use super::{LowMemoryRenderPipeline, row_buffers::RowBuffer};
+
+// Most images have at most 7 channels (RGBA + noise extra channels).
+// 8 gives a bit extra leeway and makes the size a power of two.
+pub(super) type ChannelVec<T> = SmallVec<T, 8>;
 
 fn apply_x_padding(
     input_type: DataTypeTag,
@@ -201,12 +205,10 @@ impl LowMemoryRenderPipeline {
 
                 match stage {
                     Stage::InPlace(s) => {
-                        let buffer_indices: Vec<_> = (0..num_channels)
-                            .filter(|c| s.uses_channel(*c))
-                            .map(|x| self.stage_input_buffer_index[i][x])
-                            .collect();
-                        let mut buffers =
-                            get_distinct_indices(&mut self.row_buffers, &buffer_indices);
+                        let mut buffers = get_distinct_indices(
+                            &mut self.row_buffers,
+                            &self.sorted_buffer_indices[i],
+                        );
                         s.run_stage_on(
                             ExtraInfo {
                                 xsize: shifted_xsize,
@@ -223,13 +225,9 @@ impl LowMemoryRenderPipeline {
                     }
                     Stage::Save(s) => {
                         // Find buffers for channels that will be saved.
-                        let mut input_data: Vec<_> = s
-                            .channels
+                        let mut input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
                             .iter()
-                            .map(|c| {
-                                let (si, ci) = self.stage_input_buffer_index[i][*c];
-                                &self.row_buffers[si][ci]
-                            })
+                            .map(|(si, ci)| &self.row_buffers[*si][*ci])
                             .collect();
                         // Append opaque alpha buffer if fill_opaque_alpha is set
                         if let Some(ref alpha_buf) = self.opaque_alpha_buffers[i] {
@@ -254,16 +252,12 @@ impl LowMemoryRenderPipeline {
                         let bordery = s.border().1 as isize;
                         // Apply x padding.
                         if gx == 0 && borderx != 0 {
-                            for c in 0..num_channels {
-                                if !s.uses_channel(c) {
-                                    continue;
-                                }
-                                let (si, ci) = self.stage_input_buffer_index[i][c];
+                            for (si, ci) in self.stage_input_buffer_index[i].iter() {
                                 for iy in -bordery..=bordery {
                                     let y = mirror(y as isize + iy, shifted_ysize);
                                     apply_x_padding(
                                         s.input_type(),
-                                        self.row_buffers[si][ci].get_row_mut::<u8>(y),
+                                        self.row_buffers[*si][*ci].get_row_mut::<u8>(y),
                                         -(borderx as isize)..0,
                                         // Either xsize is the actual size of the image, or it is
                                         // much larger than borderx, so this works out either way.
@@ -273,16 +267,12 @@ impl LowMemoryRenderPipeline {
                             }
                         }
                         if gx + 1 == self.shared.group_count.0 && borderx != 0 {
-                            for c in 0..num_channels {
-                                if !s.uses_channel(c) {
-                                    continue;
-                                }
-                                let (si, ci) = self.stage_input_buffer_index[i][c];
+                            for (si, ci) in self.stage_input_buffer_index[i].iter() {
                                 for iy in -bordery..=bordery {
                                     let y = mirror(y as isize + iy, shifted_ysize);
                                     apply_x_padding(
                                         s.input_type(),
-                                        self.row_buffers[si][ci].get_row_mut::<u8>(y),
+                                        self.row_buffers[*si][*ci].get_row_mut::<u8>(y),
                                         shifted_xsize as isize..(shifted_xsize + borderx) as isize,
                                         // borderx..0 is either data from the neighbouring group or
                                         // data that was filled in by the iteration above.
@@ -293,14 +283,10 @@ impl LowMemoryRenderPipeline {
                         }
                         let (inb, outb) = self.row_buffers.split_at_mut(i + 1);
                         // Prepare pointers to input and output buffers.
-                        let input_data: Vec<_> = (0..num_channels)
-                            .filter(|c| s.uses_channel(*c))
-                            .map(|c| {
-                                let (si, ci) = self.stage_input_buffer_index[i][c];
-                                &inb[si][ci]
-                            })
+                        let input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
+                            .iter()
+                            .map(|(si, ci)| &inb[*si][*ci])
                             .collect();
-                        let mut outb: Vec<_> = outb[0].iter_mut().collect();
                         s.run_stage_on(
                             ExtraInfo {
                                 xsize: shifted_xsize,
@@ -312,7 +298,7 @@ impl LowMemoryRenderPipeline {
                                 image_height: shifted_ysize,
                             },
                             &input_data,
-                            &mut outb[..],
+                            &mut outb[0][..],
                             self.local_states[i].as_deref_mut(),
                         );
                     }
@@ -354,12 +340,10 @@ impl LowMemoryRenderPipeline {
 
                 match stage {
                     Stage::InPlace(s) => {
-                        let buffer_indices: Vec<_> = (0..num_channels)
-                            .filter(|c| s.uses_channel(*c))
-                            .map(|x| self.stage_input_buffer_index[i][x])
-                            .collect();
-                        let mut buffers =
-                            get_distinct_indices(&mut self.row_buffers, &buffer_indices);
+                        let mut buffers = get_distinct_indices(
+                            &mut self.row_buffers,
+                            &self.sorted_buffer_indices[i],
+                        );
                         s.run_stage_on(
                             ExtraInfo {
                                 xsize,
@@ -376,13 +360,9 @@ impl LowMemoryRenderPipeline {
                     }
                     Stage::Save(s) => {
                         // Find buffers for channels that will be saved.
-                        let mut input_data: Vec<_> = s
-                            .channels
+                        let mut input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
                             .iter()
-                            .map(|c| {
-                                let (si, ci) = self.stage_input_buffer_index[i][*c];
-                                &self.row_buffers[si][ci]
-                            })
+                            .map(|(si, ci)| &self.row_buffers[*si][*ci])
                             .collect();
                         // Append opaque alpha buffer if fill_opaque_alpha is set
                         if let Some(ref alpha_buf) = self.opaque_alpha_buffers[i] {
@@ -405,14 +385,10 @@ impl LowMemoryRenderPipeline {
                         assert_eq!(s.border(), (0, 0));
                         let (inb, outb) = self.row_buffers.split_at_mut(i + 1);
                         // Prepare pointers to input and output buffers.
-                        let input_data: Vec<_> = (0..num_channels)
-                            .filter(|c| s.uses_channel(*c))
-                            .map(|c| {
-                                let (si, ci) = self.stage_input_buffer_index[i][c];
-                                &inb[si][ci]
-                            })
+                        let input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
+                            .iter()
+                            .map(|(si, ci)| &inb[*si][*ci])
                             .collect();
-                        let mut outb: Vec<_> = outb[0].iter_mut().collect();
                         s.run_stage_on(
                             ExtraInfo {
                                 xsize,
@@ -424,7 +400,7 @@ impl LowMemoryRenderPipeline {
                                 image_height: self.shared.input_size.1,
                             },
                             &input_data,
-                            &mut outb[..],
+                            &mut outb[0][..],
                             self.local_states[i].as_deref_mut(),
                         );
                     }

jxl/src/render/low_memory_pipeline/run_stage.rs
@@ -9,9 +9,9 @@ use crate::{
     render::{
         Channels, ChannelsMut, RunInPlaceStage,
         internal::{PipelineBuffer, RunInOutStage},
-        low_memory_pipeline::helpers::mirror,
+        low_memory_pipeline::{helpers::mirror, render_group::ChannelVec},
     },
-    util::{ShiftRightCeil, tracing_wrappers::*},
+    util::{ShiftRightCeil, SmallVec, tracing_wrappers::*},
 };
 
 use super::{
@@ -56,7 +56,7 @@ impl<T: RenderPipelineInPlaceStage> RunInPlaceStage<RowBuffer> for T {
         let xpre = if is_first_xgroup { 0 } else { out_extra_x };
         let xstart = x0 - xpre;
         let xend = x0 + xsize + if is_last_xgroup { 0 } else { out_extra_x };
-        let mut rows: Vec<_> = buffers
+        let mut rows: ChannelVec<_> = buffers
             .iter_mut()
             .map(|x| &mut x.get_row_mut::<T::Type>(current_row)[xstart..])
             .collect();
@@ -84,7 +84,7 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<RowBuffer> for T {
             is_last_xgroup,
         }: ExtraInfo,
         input_buffers: &[&RowBuffer],
-        output_buffers: &mut [&mut RowBuffer],
+        output_buffers: &mut [RowBuffer],
         state: Option<&mut dyn Any>,
     ) {
         let ibordery = Self::BORDER.1 as isize;
@@ -106,7 +106,7 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<RowBuffer> for T {
         // Build flat input rows: all rows for all channels in one Vec
         let input_rows_per_channel = (2 * Self::BORDER.1 + 1) as usize;
         let num_channels = input_buffers.len();
-        let mut input_row_data = Vec::with_capacity(num_channels * input_rows_per_channel);
+        let mut input_row_data = SmallVec::new();
         for x in input_buffers.iter() {
             for iy in -ibordery..=ibordery {
                 input_row_data.push(
@@ -120,13 +120,21 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<RowBuffer> for T {
         // Build flat output rows: all rows for all channels in one Vec
         let output_rows_per_channel = 1 << T::SHIFT.1;
         let num_output_channels = output_buffers.len();
-        let mut output_row_data = Vec::with_capacity(num_output_channels * output_rows_per_channel);
-        for x in output_buffers.iter_mut() {
-            let rows = x.get_rows_mut::<T::OutputT>(
-                (current_row << T::SHIFT.1)..((current_row + 1) << T::SHIFT.1),
-                RowBuffer::x0_offset::<T::OutputT>() - (xpre << T::SHIFT.0),
-            );
-            output_row_data.extend(rows);
+        let mut output_row_data = SmallVec::new();
+        // optimize for the common case of a single output row per channel.
+        if output_rows_per_channel == 1 {
+            for x in output_buffers.iter_mut() {
+                let row = x.get_row_mut::<T::OutputT>(current_row);
+                output_row_data.push(&mut row[xstart..]);
+            }
+        } else {
+            for x in output_buffers.iter_mut() {
+                let rows = x.get_rows_mut::<T::OutputT>(
+                    (current_row << T::SHIFT.1)..((current_row + 1) << T::SHIFT.1),
+                    RowBuffer::x0_offset::<T::OutputT>() - (xpre << T::SHIFT.0),
+                );
+                output_row_data.extend_sv(rows);
+            }
         }
         let mut output_rows = ChannelsMut::new(
             output_row_data,

jxl/src/render/simple_pipeline/mod.rs
@@ -79,12 +79,14 @@ impl SimpleRenderPipeline {
                         .filter(|x| stage.uses_channel(x.0))
                         .map(|x| x.1)
                         .collect();
-                    let mut output_buf: Vec<_> = output_buffers
-                        .iter_mut()
-                        .enumerate()
-                        .filter(|x| stage.uses_channel(x.0))
-                        .map(|x| x.1)
-                        .collect();
+                    let mut output_buf = vec![];
+                    for (c, buf) in output_buffers.iter_mut().enumerate() {
+                        if stage.uses_channel(c) {
+                            let mut tmp = Image::new((0, 0)).unwrap();
+                            std::mem::swap(&mut tmp, buf);
+                            output_buf.push(tmp);
+                        }
+                    }
                     let mut state = stage.init_local_state()?;
                     stage.run_stage_on(
                         self.shared.chunk_size,
@@ -92,6 +94,12 @@ impl SimpleRenderPipeline {
                         &mut output_buf,
                         state.as_deref_mut(),
                     );
+                    let repl_iter = (0..self.shared.num_channels())
+                        .filter(|c| stage.uses_channel(*c))
+                        .zip(output_buf.into_iter());
+                    for (c, chan) in repl_iter {
+                        output_buffers[c] = chan;
+                    }
                 }
                 Stage::InPlace(stage) => {
                     let mut output_buf: Vec<_> = output_buffers

jxl/src/render/simple_pipeline/run_stage.rs
@@ -13,7 +13,7 @@ use crate::{
         RenderPipelineInOutStage, RenderPipelineInPlaceStage, RunInOutStage, RunInPlaceStage,
         internal::PipelineBuffer,
     },
-    util::{round_up_size_to_cache_line, tracing_wrappers::*},
+    util::{SmallVec, round_up_size_to_cache_line, tracing_wrappers::*},
 };
 
 impl PipelineBuffer for Image<f64> {
@@ -71,7 +71,7 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<Image<f64>> for T {
         &self,
         chunk_size: usize,
         input_buffers: &[&Image<f64>],
-        output_buffers: &mut [&mut Image<f64>],
+        output_buffers: &mut [Image<f64>],
         mut state: Option<&mut dyn Any>,
     ) {
         assert_ne!(chunk_size, 0);
@@ -156,45 +156,47 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<Image<f64>> for T {
                         }
                     }
                 }
-                // Build flat input rows: all rows for all channels in one Vec
-                let num_input_channels = buffer_in.len();
-                let input_rows_per_channel = buffer_in[0].len();
-                let mut input_row_data =
-                    Vec::with_capacity(num_input_channels * input_rows_per_channel);
-                for ch_buf in buffer_in.iter() {
-                    for row in ch_buf.iter() {
-                        input_row_data.push(row as &[_]);
-                    }
-                }
-                let input_rows = crate::render::Channels::new(
-                    input_row_data,
-                    num_input_channels,
-                    input_rows_per_channel,
-                );
 
-                // Build flat output rows: all rows for all channels in one Vec
-                let num_output_channels = buffer_out.len();
-                let output_rows_per_channel = buffer_out[0].len();
-                let mut output_row_data =
-                    Vec::with_capacity(num_output_channels * output_rows_per_channel);
-                for ch_buf in buffer_out.iter_mut() {
-                    for row in ch_buf.iter_mut() {
-                        output_row_data.push(row as &mut [_]);
+                {
+                    // Build flat input rows: all rows for all channels in one Vec
+                    let num_input_channels = buffer_in.len();
+                    let input_rows_per_channel = buffer_in[0].len();
+                    let mut input_row_data = SmallVec::new();
+                    for ch_buf in buffer_in.iter() {
+                        for row in ch_buf.iter() {
+                            input_row_data.push(row as &[_]);
+                        }
                     }
-                }
-                let mut output_rows = crate::render::ChannelsMut::new(
-                    output_row_data,
-                    num_output_channels,
-                    output_rows_per_channel,
-                );
+                    let input_rows = crate::render::Channels::new(
+                        input_row_data,
+                        num_input_channels,
+                        input_rows_per_channel,
+                    );
 
-                self.process_row_chunk(
-                    (x, y),
-                    xsize,
-                    &input_rows,
-                    &mut output_rows,
-                    state.as_deref_mut(),
-                );
+                    // Build flat output rows: all rows for all channels in one Vec
+                    let num_output_channels = buffer_out.len();
+                    let output_rows_per_channel = buffer_out[0].len();
+                    let mut output_row_data = SmallVec::new();
+                    for ch_buf in buffer_out.iter_mut() {
+                        for row in ch_buf.iter_mut() {
+                            output_row_data.push(row as &mut [_]);
+                        }
+                    }
+                    let mut output_rows = crate::render::ChannelsMut::new(
+                        output_row_data,
+                        num_output_channels,
+                        output_rows_per_channel,
+                    );
+
+                    self.process_row_chunk(
+                        (x, y),
+                        xsize,
+                        &input_rows,
+                        &mut output_rows,
+                        state.as_deref_mut(),
+                    );
+                }
+
                 let stripe_xsize =
                     (xsize << Self::SHIFT.0).min(output_size.0 - (x << Self::SHIFT.0));
                 let stripe_ysize =

jxl/src/util/mod.rs
@@ -16,6 +16,7 @@ mod log2;
 pub mod ndarray;
 mod rational_poly;
 mod shift_right_ceil;
+mod smallvec;
 pub mod tracing_wrappers;
 mod vec_helpers;
 mod xorshift128plus;
@@ -30,5 +31,6 @@ pub use log2::*;
 pub(crate) use ndarray::*;
 pub use rational_poly::*;
 pub use shift_right_ceil::*;
+pub use smallvec::*;
 pub use vec_helpers::*;
 pub use xorshift128plus::*;

jxl/src/render/channels.rs
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+use crate::util::SmallVec;
+
 /// Multi-row channel accessor for immutable access.
 ///
 /// Provides 2D indexing where `channels[ch]` returns `&[&[T]]` (all rows for a channel),
@@ -10,7 +12,8 @@
 ///
 /// This eliminates nested Vec collections while maintaining the same indexing syntax.
 pub struct Channels<'a, T> {
-    pub(crate) row_data: Vec<&'a [T]>,
+    // The number of input rows should be maximized by the EPF0 stage, which has 21.
+    pub(crate) row_data: SmallVec<&'a [T], 32>,
     num_channels: usize,
     pub(crate) rows_per_channel: usize,
 }
@@ -22,7 +25,11 @@ impl<'a, T> Channels<'a, T> {
     /// * `row_data` - Flat vector of all rows for all channels (length = num_channels * rows_per_channel)
     /// * `num_channels` - Number of channels
     /// * `rows_per_channel` - Number of rows per channel (typically 2*BORDER+1)
-    pub fn new(row_data: Vec<&'a [T]>, num_channels: usize, rows_per_channel: usize) -> Self {
+    pub fn new(
+        row_data: SmallVec<&'a [T], 32>,
+        num_channels: usize,
+        rows_per_channel: usize,
+    ) -> Self {
         debug_assert_eq!(
             row_data.len(),
             num_channels * rows_per_channel,
@@ -66,7 +73,8 @@ impl<'a, T> std::ops::Index<usize> for Channels<'a, T> {
 /// Provides 2D indexing where `channels[ch]` returns `&[&mut [T]]` or `&mut [&mut [T]]`,
 /// and `channels[ch][row]` returns `&mut [T]` (pixels for a specific row).
 pub struct ChannelsMut<'a, T> {
-    pub(crate) row_data: Vec<&'a mut [T]>,
+    // The number of output rows should be maximized by the Upsample8 stage, which has 8.
+    pub(crate) row_data: SmallVec<&'a mut [T], 8>,
     num_channels: usize,
     pub(crate) rows_per_channel: usize,
 }
@@ -78,7 +86,11 @@ impl<'a, T> ChannelsMut<'a, T> {
     /// * `row_data` - Flat vector of all mutable rows for all channels
     /// * `num_channels` - Number of channels
     /// * `rows_per_channel` - Number of rows per channel (typically 1 << SHIFT)
-    pub fn new(row_data: Vec<&'a mut [T]>, num_channels: usize, rows_per_channel: usize) -> Self {
+    pub fn new(
+        row_data: SmallVec<&'a mut [T], 8>,
+        num_channels: usize,
+        rows_per_channel: usize,
+    ) -> Self {
         debug_assert_eq!(
             row_data.len(),
             num_channels * rows_per_channel,

commit d2f4b8ea9b58eaa7054bc0526fdc54da8dd4cc29

Fix rendering bugs introduced in PR #577
This fixes two bugs in the low_memory_pipeline introduced by the SmallVec
optimization PR:

1. SaveStage channel ordering (mod.rs):
   The optimization changed SaveStage to iterate through stage_input_buffer_index
   directly instead of using s.channels. This lost the channel reordering needed
   for BGRA output format.

   Fix: Reorder stage_input_buffer_index at construction time for SaveStage
   using s.channels, so the direct iteration in render_group.rs works correctly.

2. InOut stage output offset (run_stage.rs):
   The optimized path for output_rows_per_channel == 1 used xstart (based on
   x0_offset::<InputT>()) but should use x0_offset::<OutputT>(). For stages that
   convert between types (e.g., f32→u8), these offsets differ significantly
   (16 vs 64 elements for cache line padding), causing output to be written at
   wrong positions and resulting in black bars in the rendered image.

jxl/src/render/low_memory_pipeline/mod.rs
@@ -356,9 +356,16 @@ impl RenderPipeline for LowMemoryRenderPipeline {
             }
         }
 
+        let default_channels: Vec<usize> = (0..nc).collect();
         for (s, ibi) in stage_input_buffer_index.iter_mut().enumerate() {
             let mut filtered = vec![];
-            for c in 0..nc {
+            // For SaveStage, use s.channels to get correct output ordering (e.g., BGRA).
+            let channels = if let Stage::Save(save_stage) = &shared.stages[s] {
+                save_stage.channels.as_slice()
+            } else {
+                default_channels.as_slice()
+            };
+            for &c in channels {
                 if shared.stages[s].uses_channel(c) {
                     filtered.push(ibi[c]);
                 }

jxl/src/render/low_memory_pipeline/render_group.rs
@@ -225,6 +225,7 @@ impl LowMemoryRenderPipeline {
                     }
                     Stage::Save(s) => {
                         // Find buffers for channels that will be saved.
+                        // Channel ordering is handled in stage_input_buffer_index construction.
                         let mut input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
                             .iter()
                             .map(|(si, ci)| &self.row_buffers[*si][*ci])
@@ -360,6 +361,7 @@ impl LowMemoryRenderPipeline {
                     }
                     Stage::Save(s) => {
                         // Find buffers for channels that will be saved.
+                        // Channel ordering is handled in stage_input_buffer_index construction.
                         let mut input_data: ChannelVec<_> = self.stage_input_buffer_index[i]
                             .iter()
                             .map(|(si, ci)| &self.row_buffers[*si][*ci])

jxl/src/render/low_memory_pipeline/run_stage.rs
@@ -123,9 +123,12 @@ impl<T: RenderPipelineInOutStage> RunInOutStage<RowBuffer> for T {
         let mut output_row_data = SmallVec::new();
         // optimize for the common case of a single output row per channel.
         if output_rows_per_channel == 1 {
+            // Use OutputT's x0_offset, not InputT's - they differ for type conversions (e.g., f32→u8).
+            // Must apply the same offset calculation as the else branch.
+            let output_xstart = RowBuffer::x0_offset::<T::OutputT>() - (xpre << T::SHIFT.0);
             for x in output_buffers.iter_mut() {
                 let row = x.get_row_mut::<T::OutputT>(current_row);
-                output_row_data.push(&mut row[xstart..]);
+                output_row_data.push(&mut row[output_xstart..]);
             }
         } else {
             for x in output_buffers.iter_mut() {