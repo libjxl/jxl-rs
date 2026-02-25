2f58bddbae9359cd0d381b4fbfc7c68b0ed5e977
Luca Versari <veluca93@gmail.com>
2026-02-25 16:38:56 +0100

    Ensure row buffers have enough space when doing x upsampling.

--- jxl/src/render/low_memory_pipeline/row_buffers.rs
+++ jxl/src/render/low_memory_pipeline/row_buffers.rs
@@ -33,13 +33,15 @@ impl RowBuffer {
         data_type: DataTypeTag,
         next_y_border: usize,
         y_shift: usize,
+        x_shift: usize,
         row_len: usize,
     ) -> Result<Self> {
         let num_rows = (1 << y_shift) + 2 * next_y_border;
         let num_rows = num_rows.next_power_of_two();
         // Input offset is at *one* cacheline, and we need up to *two* cachelines on the other
         // side as the data might exceed xsize slightly.
-        let row_stride = (row_len * data_type.size()).div_ceil(CACHE_LINE_BYTE_SIZE) + 3;
+        let row_stride =
+            (row_len * data_type.size()).div_ceil(CACHE_LINE_BYTE_SIZE) + (3 << x_shift);
         let mut buffer = Vec::<CacheLine>::new();
         buffer.try_reserve_exact(row_stride * num_rows)?;
         buffer.resize(row_stride * num_rows, CacheLine::default());
@@ -54,7 +56,7 @@ impl RowBuffer {
     /// Creates a new row buffer with a single row filled with a repeating pattern.
     /// Used for constant values like opaque alpha.
     pub fn new_filled(data_type: DataTypeTag, row_len: usize, fill_pattern: &[u8]) -> Result<Self> {
-        let mut result = Self::new(data_type, 0, 0, row_len)?;
+        let mut result = Self::new(data_type, 0, 0, 0, row_len)?;
         let row_bytes: &mut [u8] = result.get_row_mut(0);
 
         // Fill the *entire* allocated row, including the padding on both sides,
