8aa4b5100bae26c2c4d5596da503b43424edc61d
mertalev <101130780+mertalev@users.noreply.github.com>
2026-03-16 21:19:14 -0400

    replace dispatch with cfg

--- jxl_simd/src/wasm32/mod.rs
+++ jxl_simd/src/wasm32/mod.rs
@@ -21,28 +21,12 @@ macro_rules! simd_function {
         $pub fn $dname($($arg: $ty),*) $(-> $ret)? {
             #[allow(unused)]
             use $crate::SimdDescriptor;
-            $crate::simd_function_body_simd128!($name($($arg: $ty),*) $(-> $ret)?; ($($arg),*));
-            $name($crate::ScalarDescriptor::new().unwrap(), $($arg),*)
+            #[cfg(all(feature = "simd128", target_feature = "simd128"))]
+            { $name($crate::Simd128Descriptor::new().unwrap(), $($arg),*) }
+            #[cfg(not(all(feature = "simd128", target_feature = "simd128")))]
+            { $name($crate::ScalarDescriptor::new().unwrap(), $($arg),*) }
         }
     };
-}
-
-#[cfg(feature = "simd128")]
-#[doc(hidden)]
-#[macro_export]
-macro_rules! simd_function_body_simd128 {
-    ($name:ident($($arg:ident: $ty:ty),* $(,)?) $(-> $ret:ty )?; ($($val:expr),* $(,)?)) => {
-        if let Some(d) = $crate::Simd128Descriptor::new() {
-            return $name(d, $($val),*);
-        }
-    };
-}
-
-#[cfg(not(feature = "simd128"))]
-#[doc(hidden)]
-#[macro_export]
-macro_rules! simd_function_body_simd128 {
-    ($($ignore:tt)*) => {};
 }
 
 #[macro_export]
@@ -92,13 +76,13 @@ macro_rules! bench_all_instruction_sets {
         $criterion:ident
     ) => {
         use $crate::SimdDescriptor;
-        // `simd_function_body_*` does early return; wrap it with an immediately-invoked closure
-        (|| {
-            $crate::simd_function_body_simd128!(
-                $name($criterion: &mut ::criterion::BenchmarkGroup<'_, impl ::criterion::measurement::Measurement>);
-                ($criterion, "simd128")
-            );
-        })();
+        #[cfg(all(feature = "simd128", target_feature = "simd128"))]
+        $name(
+            $crate::Simd128Descriptor::new().unwrap(),
+            $criterion,
+            "simd128",
+        );
+        #[cfg(not(all(feature = "simd128", target_feature = "simd128")))]
         $name(
             $crate::ScalarDescriptor::new().unwrap(),
             $criterion,
