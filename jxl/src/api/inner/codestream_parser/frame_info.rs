856844d2cb82df1d6d54d0582bbbb511a0810de4
Luca Versari <veluca93@gmail.com>
2026-07-30 16:58:20 +0200

    Mark where more parallelization would be helpful

--- jxl/src/api/inner/codestream_parser/frame_info.rs
+++ jxl/src/api/inner/codestream_parser/frame_info.rs
@@ -488,6 +488,7 @@ impl FrameInfo {
             return Ok(data_for_next_section);
         }
 
+        // TODO(veluca): parallelize here
         for lf_section in self.lf_sections.drain(..) {
             let Section::Lf { group } = lf_section.section else {
                 unreachable!()
