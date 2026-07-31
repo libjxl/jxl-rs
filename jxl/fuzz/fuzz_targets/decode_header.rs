45be1e5fb41614fff2ae3047744280ead09aab33
Luca Versari <veluca93@gmail.com>
2026-07-31 13:43:42 +0200

    Add a fuzzer for parallel decoding

--- jxl/fuzz/fuzz_targets/decode_header.rs
+++ jxl/fuzz/fuzz_targets/decode_header.rs
@@ -8,9 +8,8 @@ use jxl::api::{JxlDecoder, JxlDecoderOptions, states};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    // Note: cargo-fuzz doesn't support |mut data: &[u8]| here.
     let mut data = data;
     let decoder_options = JxlDecoderOptions::default();
     let initialized_decoder = JxlDecoder::<states::Initialized>::new(decoder_options);
-    let _ = initialized_decoder.process(&mut data);
+    let _ = initialized_decoder.process(&mut data, None);
 });
